a5d60c708d6153710c24c0ae7a6332b55d24f1ff
Daniel Visser <daniel_visser95@outlook.com>
2024-06-19 20:29:37 +0100

    items wip

--- apps/items/models.py
+++ apps/items/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    class meta:
+        db_table = "items"
+
+    STATUS_CHOICES = (
+        ("registered", "Registered"),
+        ("listed", "Listed"),
+        ("recalled", "Recalled"),
+    )
+
+    member = models.ForeignKey("members.MemberProfile", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    condition = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default="registered")
+    
+
+# Junction table to join with categories for each item
+class ItemCategory(models.Model):
+    class Meta:
+        db_table = "item_categories"
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    category = models.ForeignKey("items.Category", on_delete=models.CASCADE)
+  
+
+class Categories(models.Model):
+    class Meta:
+        db_table = "categories"
+
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
